35de825ed05051ed473c003ad39859f90d0091d0
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-07-12 18:14:58 +0100

    Week 3 extended.

--- block_superframe.php
+++ block_superframe.php
@@ -85,6 +85,15 @@ class block_superframe extends block_base {
             $this->content->text .= html_writer::tag('p', html_writer::link($url, get_string('viewlink', 'block_superframe')));
         }
 
+        // List of course students.
+        $courseid = $this->page->course->id;
+        $users = self::get_course_users($courseid);
+        $this->content->text .= html_writer::start_tag('ul');
+        foreach ($users as $user) {
+            $this->content->text .= html_writer::tag('li', $user->lastname . ', ' . $user->firstname);
+        }
+        $this->content->text .= html_writer::end_tag('ul');
+
         return $this->content;
     }
 
@@ -114,5 +123,22 @@ class block_superframe extends block_base {
      */
     public function has_config() {
         return true;
+    }
+
+    private static function get_course_users($courseid) {
+        global $DB;
+
+        $sql = "SELECT u.id, u.firstname, u.lastname ";
+        $sql .= "FROM {course} c ";
+        $sql .= "JOIN {context} x ON c.id = x.instanceid ";
+        $sql .= "JOIN {role_assignments} r ON r.contextid = x.id ";
+        $sql .= "JOIN {user} u ON u.id = r.userid ";
+        $sql .= "WHERE c.id = :courseid ";
+        $sql .= "AND r.roleid = :roleid";
+
+        // In real world query should check users are not deleted/suspended.
+        $records = $DB->get_records_sql($sql, ['courseid' => $courseid, 'roleid' => 5]);
+
+        return $records;
     }
 }
